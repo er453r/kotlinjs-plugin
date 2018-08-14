a4bd578b21f620db8d12df51cbd30157a1e7438a
Marcin Kotz <marcin.kotz@ives-system.com>
2018-08-14 15:45:32 +0200

    initial commit

---
+++ app/src/com/er453r/plugintest/TestClass.kt
@@ -0,0 +1,3 @@
+package com.er453r.plugintest
+
+class TestClass
---
+++ compiler-plugin/src/com/er453r/plugin/TestExtension.kt
@@ -0,0 +1,23 @@
+package com.er453r.plugin
+
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.INFO
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.WARNING
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.codegen.ClassBuilderFactory
+import org.jetbrains.kotlin.codegen.extensions.ClassBuilderInterceptorExtension
+import org.jetbrains.kotlin.diagnostics.DiagnosticSink
+import org.jetbrains.kotlin.resolve.BindingContext
+
+class TestExtension(val messageCollector: MessageCollector) : ClassBuilderInterceptorExtension {
+
+    override fun interceptClassBuilderFactory(
+            interceptedFactory: ClassBuilderFactory,
+            bindingContext: BindingContext,
+            diagnostics: DiagnosticSink
+    ): ClassBuilderFactory {
+        messageCollector.report(WARNING, "WORKS", CompilerMessageLocation.create("Lol CODE"))
+
+        return interceptedFactory
+    }
+}
