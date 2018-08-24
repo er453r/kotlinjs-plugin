98269a00c73869fc2926c2f81ea0ae0187800906
er453r <er453r@gmail.com>
2018-08-24 12:14:01 +0200

    test on js target

--- app/src/com/er453r/plugintest/TestClass.kt
+++ app/src/com/er453r/plugintest/TestClass.kt
@@ -2,12 +2,6 @@ package com.er453r.plugintest
 
 class TestClass{
     constructor(){
-        println("derp ${TestClass::class}")
+        println("derp constructed ${TestClass::class}")
     }
-}
-
-fun main(args:Array<String>){
-    println("started")
-
-    TestClass()
 }
---
+++ app/src/com/er453r/plugintest/TestUtils.kt
@@ -0,0 +1,7 @@
+package com.er453r.plugintest
+
+class TestUtils {
+    fun sayHello(name:String):String{
+        return "Hello to you $name"
+    }
+}
